eb81c0707c2e7eb1504d0ff6db814a89ea1cd275
Siborne <904389425@qq.com>
2026-01-30 00:59:49 +0800

    feat(dish): 添加菜品批量删除功能
    - 在 DishFlavorMapper 中添加 deleteByDishIds 方法用于批量删除口味数据
    - 在 DishFlavorMapper.xml 中实现 deleteByDishIds 的 SQL 删除逻辑
    - 在 DishMapper 中添加 deleteByIds 方法用于批量删除菜品数据
    - 在 DishMapper.xml 中实现 deleteByIds 的 SQL 删除逻辑
    - 修改 DishServiceImpl 中的删除逻辑，使用批量删除方法替代循环删除
    - 优化删除操作性能，减少数据库交互次数

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -97,12 +97,21 @@ public class DishServiceImpl implements DishService {
             throw new DeletionNotAllowedException(MessageConstant.DISH_BE_RELATED_BY_SETMEAL);
         }
 
-        for (Long id : ids){
+        /*for (Long id : ids){
             //删除菜品表中的菜品数据
             dishMapper.deleteById(id);
             //删除菜品关联中的口味数据
             dishFlavorMapper.deleteByDishId(id);
-        }
+        }*/
+
+        //根据菜品id集合批量删除菜品数据
+        //sql: delete from dish where id in (?,?,?)
+        dishMapper.deleteByIds(ids);
+
+        //根据菜品id集合批量删除菜品关联表中的数据
+        //sql: delete from dish_flavor where dish_id in (?,?,?)
+        dishFlavorMapper.deleteByDishIds(ids);
+
     }
 
 }
